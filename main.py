727455f2d73b2127887fc2c1c3be87edc365d2b5
AbhimanyuPratapSingh1803 <abhiheller1803@gmail.com>
2025-01-18 11:41:04 +0530

    changed host

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.include_router(user_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, port=8000, reload=True)
 
 @app.get("/")
 def read_root():
